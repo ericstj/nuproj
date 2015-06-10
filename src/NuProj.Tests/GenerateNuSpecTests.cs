96178da73f44bba457382047458d958fc3080fc0
Eric St. John <ericstj@microsoft.com>
2015-06-10 14:11:14 -0700

    Update to nuget 2.8.6

--- src/NuProj.Tests/GenerateNuSpecTests.cs
+++ src/NuProj.Tests/GenerateNuSpecTests.cs
@@ -152,7 +152,7 @@ namespace NuProj.Tests
                             new ManifestDependency
                             {
                                 Id = "NuGet.Core",
-                                Version = "2.8.5"
+                                Version = "2.8.6"
                             }
                         }
                     }
